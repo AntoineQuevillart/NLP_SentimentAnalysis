f3a9d503ac22a21aa3b9dea2842c218b21253fe3
Antoine Quevillart <antoine.quevillart@etudiant.univ-reims.fr>
2022-12-01 10:49:02 +0100

    final model

--- classification/model.py
+++ classification/model.py
@@ -1,39 +1,53 @@
 import pickle
 import joblib
-from sklearn import svm
+from sklearn.svm import SVC
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
 # Load pickle encoded dataset
-filename = "C:/dataset/encoded_dataset.pkl"
-infile = open(filename,'rb')
-dataset = pickle.load(infile)
-infile.close()
+filename_train = "C:/model/train_selected.pkl"
+filename_test = "C:/model/test_selected.pkl"
+filename_ytrain = "C:/model/ytrain_selected.pkl"
+filename_ytest = "C:/model/ytest_selected.pkl"
 
-# Train test splitting
-train, test = train_test_split(dataset, test_size=0.2)
+infile_train = open(filename_train,'rb')
+infile_test = open(filename_test,'rb')
+infile_ytrain = open(filename_ytrain,'rb')
+infile_ytest = open(filename_ytest,'rb')
 
-# Define X and y
-X_train = train[:,1:len(train)]
-y_train = train[:,0]
-X_test = test[:,1:len(test)]
-y_test = test[:,0]
+train = pickle.load(infile_train)
+test = pickle.load(infile_test)
+y_train = pickle.load(infile_ytrain)
+y_test = pickle.load(infile_ytest)
+
+infile_train.close()
+infile_test.close()
+infile_ytrain.close()
+infile_ytest.close()
 
 # Convert y type from float to integer
+y_train[y_train == "positive"] = 1
+y_train[y_train == "negative"] = 0
+y_test[y_test == "positive"] = 1
+y_test[y_test == "negative"] = 0
+
 y_train = y_train.astype('int')
 y_test = y_test.astype('int')
 
-# SVM model - 5 fold cross validation
-clf_svm = svm.SVC(probability=True)
-clf_svm.fit(X_train, y_train)
+# SVM Model
+classifierSVM = SVC(C = 1, kernel = 'rbf', gamma = 0.5, probability = True, random_state = 0)
+classifierSVM.fit(train, y_train)
 
 # Prediction
-y_pred = clf_svm.predict(X_test)
+y_pred = classifierSVM.predict(test)
+y_pred = svm_model.predict(test)
+
 
 # Model Accuracy: how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-# 78,8% accuracy
+print("Accuracy:", metrics.accuracy_score(test, y_pred))
+# 81,68% accuracy
 
 # Save the SVM classifier with joblib
-filename = "C:/model/svm_classifier.joblib"
-joblib.dump(clf_svm, filename)
+joblib.dump(value=classifierSVM, filename='scoring_model.pkl')
+
--- classification/predict.py
+++ classification/predict.py
@@ -1,17 +1,22 @@
 import joblib
 from tools.cleaning import clean_review
+import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 # Initialize encoder
 encoder = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Load the model from disk
-svm_model = joblib.load(open("C:/model/svm_classifier.joblib", 'rb'))
+# Load model from disk
+svm_model = joblib.load(filename = "C:/dataset/scoring_model.pkl")
+selection = joblib.load(filename = 'C:/dataset/select_variable.pkl')
 
 # Predict function
 def predict():
     # Asking the user to type a review
-    review = input('Please type a review: ')
+
+    review = pd.read_excel(r"C:/Users/antoi/Documents/python_vba.xlsm", sheet_name="Input")
+
+    review = str(review.dtypes)
 
     review = clean_review(review)
 
@@ -19,12 +24,21 @@ def predict():
 
     embeddings = embeddings.reshape(1, 384)
 
-    prediction = svm_model.predict(embeddings)
+    embeddings = pd.DataFrame(embeddings)
 
-    print(prediction)
+    embeddings = selection.transform(embeddings[embeddings.columns[:384]])
+
+    prediction = svm_model.predict_proba(embeddings)
+
+    output = pd.DataFrame(prediction)
+
+    output = round(output, 2)
+
+    output.to_excel(r"C:/Users/antoi/Documents/output_model.xls", sheet_name='Output', index=False)
 
 if __name__ == '__main__':
-    # Running the loop forever
     while True:
         predict()
         break
+
+
